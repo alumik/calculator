ca98677021ec159ba14e7e0d17f02a77de32c2f2
Zhong Zhenyu <nczzy1997@gmail.com>
2018-08-01 21:27:19 +0800

    Implement all functions

---
+++ src/components/Keypad.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <button class="btn-secondary" @click="btnClear()">AC</button>
+    <button class="btn-secondary" @click="btnChangeSign()">+/-</button>
+    <button class="btn-secondary" @click="btnPercentage()">%</button>
+
+    <button class="btn-operator" @click="btnOperator('÷')">÷</button>
+
+    <button class="btn-number" @click="btnNumber('7')">7</button>
+    <button class="btn-number" @click="btnNumber('8')">8</button>
+    <button class="btn-number" @click="btnNumber('9')">9</button>
+
+    <button class="btn-operator" @click="btnOperator('×')">×</button>
+
+    <button class="btn-number" @click="btnNumber('4')">4</button>
+    <button class="btn-number" @click="btnNumber('5')">5</button>
+    <button class="btn-number" @click="btnNumber('6')">6</button>
+
+    <button class="btn-operator" @click="btnOperator('-')">-</button>
+
+    <button class="btn-number" @click="btnNumber('1')">1</button>
+    <button class="btn-number" @click="btnNumber('2')">2</button>
+    <button class="btn-number" @click="btnNumber('3')">3</button>
+
+    <button class="btn-operator" @click="btnOperator('+')">+</button>
+
+    <button class="btn-large btn-number btn-bottom-left" @click="btnNumber('0')">0</button>
+    <button class="btn-number" @click="btnNumber('.')">.</button>
+
+    <button id="btn-equal" class="btn-equal btn-bottom-right" @click="btnEqual()">=</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Keypad',
+  methods: {
+    btnClear () {
+      this.$emit('btnPressed', { type: 'btn-clear', data: '' })
+    },
+
+    btnChangeSign () {
+      this.$emit('btnPressed', { type: 'btn-changesign', data: '' })
+    },
+
+    btnPercentage () {
+      this.$emit('btnPressed', { type: 'btn-percentage', data: '' })
+    },
+
+    btnNumber (number) {
+      this.$emit('btnPressed', { type: 'btn-number', data: number })
+    },
+
+    btnOperator (operator) {
+      this.$emit('btnPressed', { type: 'btn-operator', data: operator })
+    },
+
+    btnEqual () {
+      this.$emit('btnPressed', { type: 'btn-equal', data: '' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  @font-face{
+    font-weight: normal;
+    font-style: normal;
+    font-family: 'helvetica_neue';
+    src: url('../assets/fonts/helveticaneue-webfont.eot');
+    src: url('../assets/fonts/helveticaneue-webfont.eot') format('embedded-opentype'),
+    url('../assets/fonts/helveticaneue-webfont.woff') format('woff'),
+    url('../assets/fonts/helveticaneue-webfont.ttf') format('truetype'),
+    url('../assets/fonts/helveticaneue-webfont.svg') format('svg');
+  }
+
+  div {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+  }
+
+  button {
+    margin: 1px;
+    height: 80px;
+    text-align: center;
+    font-family: 'helvetica_neue', Helvetica, Arial, sans-serif;
+    cursor: pointer;
+    flex: 1 1 24%;
+    border-style: none;
+  }
+
+  button:focus {
+    outline: none;
+  }
+
+  .btn-operator, .btn-equal {
+    background-color: #f69232;
+    color: #fff;
+    font-size: 48px;
+    font-family: "Courier New", Courier, monospace;
+  }
+
+  .btn-operator:active, .btn-equal:active {
+    background-color: #ba7737;
+    color: #000;
+  }
+
+  .btn-secondary {
+    background-color: #d6d6d6;
+    font-size: 32px;
+  }
+
+  .btn-secondary:active {
+    background-color: #a9a9a9;
+  }
+
+  .btn-number {
+    background-color: #e0e0e0;
+    font-size: 36px;
+  }
+
+  .btn-number:active {
+    background-color: #b2b2b2;
+  }
+
+  .btn-large {
+    padding-right: calc(24% + 8px);
+    flex: 1 1 49%;
+  }
+
+  .btn-bottom-left {
+    border-radius: 0 0 0 9px;
+  }
+
+  .btn-bottom-right {
+    border-radius: 0 0 9px 0;
+  }
+</style>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="btn-round btn-round-red"></div>
+    <div class="btn-round btn-round-yellow"></div>
+    <div class="btn-round btn-round-green"></div>
+    <div ref="keyboardIndicator" id="keyboard-indicator">KEYBOARD OFF</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleBar'
+}
+</script>
+
+<style scoped>
+  div {
+    box-sizing: border-box;
+    display: flex;
+    width: 100%;
+    box-shadow: none;
+    margin: 0;
+  }
+
+  .btn-round {
+    margin-top: 10px;
+    margin-left: 15px;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+  }
+
+  .btn-round-red {
+    background-color: #fe534d;
+  }
+
+  .btn-round-yellow {
+    background-color: #ffbf2f;
+  }
+
+  .btn-round-green {
+    background-color: #28c93f;
+  }
+
+  .btn-round-red:active {
+    background-color: #cd4c46;
+  }
+
+  .btn-round-yellow:active {
+    background-color: #cd962f;
+  }
+
+  .btn-round-green:active {
+    background-color: #219732;
+  }
+
+  #keyboard-indicator {
+    display: inline-block;
+    margin: 10px 15px 0 0;
+    color: #bbb;
+    text-align: right;
+    font-weight: bold;
+    font-size: 18px;
+    flex: 1 0;
+  }
+</style>
